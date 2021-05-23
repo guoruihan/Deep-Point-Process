532680eaf62740201359ea88f884db703067fadf
Ruihan Guo <guoruihan@sjtu.edu.cn>
2021-05-23 15:50:53 +0800

    finish try json

--- tryJson.py
+++ tryJson.py
@@ -1,8 +1,20 @@
 import argparse
 import json
 
-if __name__ == "main":
+
+if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
-        "--config_path", default = "./hw1/config/bivariate.json", help = "config saved path"
+        "--config_path", default = "./hw1/config/univariate.json", help = "config saved path"
     )
+
+    argparser.add_argument(
+        "--save_path", default= "./hw1/results/", help="where to save simulation results"
+    )
+
+    args = argparser.parse_args()
+
+    with open(args.config_path) as f:
+        config = json.load(f)
+
+    print(config)
